64a9349d7c173d748771f78dbefae5f956007961
Kien <vongkientran@gmail.com>
2024-10-01 08:41:57 -0700

    finish version 1 and continue to work on version 2

--- modifiedFulianDate.cpp
+++ modifiedFulianDate.cpp
@@ -1,5 +1,68 @@
+// Program    : Modified Julian Date
+// Author	  : Kien Tran Vong
+// Date		  : Fall 2024
+// Description: This program will promt user to input date, month, and year, 
+//				then computes and displays the corresponding MJD.
+
 #include <iostream>
+#include <iomanip>
+
+
+void user_input(int& ngay, int& thang, int& nam);
+void caculate_all_num(int& n1, int& n2, int& n3, int& n4,
+					  int& mjd, int month, int day, int year);
+void print_result(int day, int month, int year, int MJD);
 
 int main() {
-	std::cout << "hello world";
+	int month,		//
+		day,		//input by user
+		year,		//
+		n1, 
+		n2, 
+		n3, 
+		n4, 
+		mjd;		//the corresponding MJD
+	user_input(day, month, year);
+	caculate_all_num(n1, n2, n3, n4, mjd, month, day, year);
+	print_result(day, month, year, mjd);
+
 }
+
+//
+//This module will get input from user(day, month, year)
+//
+void user_input(int &ngay, int &thang, int &nam) {
+	std::cout << "Enter a month number (Jan=1, Feb=2,etc.): ";
+	std::cin >> thang;
+	std::cout << "Enter a day number (1...31) " << std::setw(14)<< " : ";
+	std::cin >> ngay;
+	std::cout << "Enter a year using four digits " << std::setw(11)<< " : ";
+	std::cin >> nam;
+}	
+
+//
+//This module caculate n1...n4 and mjd number
+//
+void caculate_all_num(int &n1, int &n2, int &n3, int &n4,
+					  int &mjd, int month, int day, int year) 
+{
+	n1 =(14-month)/12;
+	n2 = (month - 3) + (12 * n1);
+	n3 = year + 4800 - n1;			
+	n4 = (n3 / 4) - (n3 / 100) + (n3 / 400);						
+	mjd = day + (((153 * n2) + 2) / 5) + (365 * n3) + n4 - 2432046;
+}
+
+//
+//This module will print out the result
+//
+void print_result(int day, int month, int year, int MJD) {
+	std::cout << std::endl;
+	std::cout << "The MJD for " 
+		<< month << "/" 
+		<< day << "/" 
+		<< year 
+		<< " is " 
+		<< MJD;
+}
+
---
+++ newModifiedJulianDate.cpp
@@ -0,0 +1,97 @@
+#include<iostream>
+#include<iomanip>
+
+void get_date(int& month, int& day, int& year);
+void compute_mjd(int month, int day, int year, int& mjd);
+void compute_dow(int month, int day, int year, int& dow);
+void compute_days(int mjd1, int mjd2, int& days_between);
+
+int main() {
+	int today_month,
+		today_day,
+		today_year,
+		today_mjd;
+	int birthday_month,
+		birthday_day,
+		birthday_year,
+		birthday_mjd;
+	int birthday_dow,
+		days_between;
+	std::cout << "For today's date..." << std::endl;
+	get_date(today_month, today_day, today_year);
+	std::cout << "For your next birthday..." << std::endl;
+	get_date(birthday_month, birthday_day, birthday_year);
+	compute_mjd(today_month, today_day, today_year, today_mjd);
+	compute_mjd(birthday_month, birthday_day, birthday_year, birthday_mjd);
+
+
+
+	/*
+	Declare today_month, today_day, today_year, today_mjd as Integer
+Declare birthday_month, birthday_day, birthday_year, birthday_mjd as Integer
+Declare birthday_dow, days_between as Integer
+Write "For today's date..."
+Call get_date( today_month, today_day, today_year )
+Write "For your next birthday..."
+Call get_date( birthday_month, birthday_day, birthday_year )
+Call compute_mjd( today_month, today_day, today_year, today_mjd )
+Call compute_mjd( birthday_month, birthday_day, birthday_year, birthday_mjd )
+Call compute_dow( birthday_month, birthday_day, birthday_year, birthday_dow )
+Call compute_days( today_mjd, birthday_mjd, days_between )
+Call display_results( today_month, today_day, today_year, today_mjd, birthday_month, birthday_day, birthday_year, birthday_mjd, birthday_dow, days_between)*/
+
+}
+
+//
+// This module will get date from the user..
+// 
+void get_date(int &month, int &day,int &year) {
+	std::cout << "Enter a month number (Jan=1, Feb=2,etc.): ";
+	std::cin >> month;
+	std::cout << "Enter a day number (1...31) " << std::setw(14) << " : ";
+	std::cin >> day;
+	std::cout << "Enter a year using four digits " << std::setw(11) << " : ";
+	std::cin >> year;
+	
+}
+
+//
+//This module caculate n1...n4 and mjd number
+//
+void compute_mjd( int month, int day, int year, int &mjd)
+{
+	int n1, n2, n3, n4;
+	n1 = (14 - month) / 12;
+	n2 = (month - 3) + (12 * n1);
+	n3 = year + 4800 - n1;
+	n4 = (n3 / 4) - (n3 / 100) + (n3 / 400);
+	mjd = day + (((153 * n2) + 2) / 5) + (365 * n3) + n4 - 2432046;
+}
+//
+// This module computes day of week(dow)
+//
+void compute_dow(int month, int day, int year, int &dow) {
+	int mjd;
+	compute_mjd(month, day, year, mjd);
+	dow = (mjd + 3) % 7;
+}
+
+//
+// This module computes the number of days between two given MJDs
+//
+void compute_days(int mjd1, int mjd2, int& days_between) {
+	days_between = mjd1 - mjd1;
+}
+
+//
+// This module displays date
+//
+void display_date(int month, int day, int year) {
+	std::cout << month << "/" << day << "/" << year;
+}
+
+void display_results(int today_month, int today_day, int today_year, 
+					int today_mjd, int birthday_month, int birthday_day, int birthday_year,
+					int birthday_mjd, int birthday_dow,int days_between) {
+
+}
